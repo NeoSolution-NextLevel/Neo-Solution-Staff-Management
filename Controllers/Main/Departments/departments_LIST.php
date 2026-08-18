fc87e1f55f4bc322641019761fc2f3a9d16d0044
devindirathuge <devindineosolution@gmail.com>
2026-08-18 08:42:39 +0530

    Create departments_LIST.php

---
+++ Controllers/Main/Departments/departments_LIST.php
@@ -0,0 +1,69 @@
+<?php
+if (!class_exists('DataBase')) {
+    if (file_exists(__DIR__ . '/../../../imports/need/DB.php')) {
+        include_once __DIR__ . '/../../../imports/need/DB.php';
+    } elseif (file_exists(__DIR__ . '/../../../database.php')) {
+        include_once __DIR__ . '/../../../database.php';
+    }
+}
+
+class departments_LIST
+{
+    private $sql_search_data = "";
+    private $sql_process_data = "*";
+    private $pagination_data_result = "";
+    private $ast_state = "1";
+
+    public function filter_by_ast($get_ast)
+    {
+        $this->ast_state = $get_ast;
+        $this->sql_search_data .= " AND ast='" . $this->ast_state . "'";
+    }
+
+    public function filter_by_id($get_id)
+    {
+        $this->sql_search_data .= " AND id='" . $get_id . "'";
+    }
+
+    public function filter_by_name($get_name)
+    {
+        $this->sql_search_data .= " AND name='" . $get_name . "'";
+    }
+
+    public function filter_by_head($get_head)
+    {
+        $this->sql_search_data .= " AND head='" . $get_head . "'";
+    }
+
+    public function filter_by_search($get_search)
+    {
+        $this->sql_search_data .= " AND (name LIKE '%" . $get_search . "%' OR head LIKE '%" . $get_search . "%')";
+    }
+
+    public function get_count_report()
+    {
+        $this->sql_process_data = "COUNT(id)";
+    }
+
+    public function set_data_limits($start_point, $per_page_data_count)
+    {
+        $this->pagination_data_result = " ORDER BY id DESC LIMIT " . $start_point . ", " . $per_page_data_count;
+    }
+
+    public function remove_list()
+    {
+        $this->ast_state = "0";
+    }
+
+    public function get_result()
+    {
+        $data_base_obj = new DataBase();
+        $get_sql_query = "SELECT " . $this->sql_process_data .
+            " FROM departments WHERE ast='" . $this->ast_state . "'" .
+            $this->sql_search_data .
+            $this->pagination_data_result;
+
+        return $data_base_obj->get_result($get_sql_query);
+    }
+}
+
