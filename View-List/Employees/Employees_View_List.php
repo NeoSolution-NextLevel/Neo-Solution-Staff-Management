d0f9726201e24da7b40f39cc3f967a7943ff2901
devindirathuge <devindineosolution@gmail.com>
2026-08-23 14:11:35 +0530

    Create Employees_View_List.php

---
+++ View-List/Employees/Employees_View_List.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * View-List / Employees / Employees_View_List.php
+ * Handles server-side fetching and listing of employee records.
+ */
+include_once __DIR__ . '/../../UxUi-Back/Employee/fetch_employee/fetch_employee.php';
