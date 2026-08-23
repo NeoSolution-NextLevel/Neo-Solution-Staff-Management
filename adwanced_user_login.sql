24106b521cc9de79b1138c12cedc6d4d87b8251c
devindirathuge <devindineosolution@gmail.com>
2026-08-23 15:17:59 +0530

    Update adwanced_user_login.sql

--- adwanced_user_login.sql
+++ adwanced_user_login.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `adwanced_user_login`
+Database: `user_login_db`
 --
 
 -- --------------------------------------------------------
