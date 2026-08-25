82e9c510975e1f081b7a6755efaa0cb0dd05adbe
devindirathuge <devindineosolution@gmail.com>
2026-08-25 18:44:09 +0530

    Update fetch_job_roles.php

--- UxUi-Back/Job_Roles/fetch_job_roles/fetch_job_roles.php
+++ UxUi-Back/Job_Roles/fetch_job_roles/fetch_job_roles.php
@@ -4,15 +4,6 @@ header('Content-Type: application/json; charset=utf-8');
 include_once __DIR__ . '/../../../imports/need/DB.php';
 include_once __DIR__ . '/../../../Controllers/Main/Job_Roles/job_roles_details_LIST.php';
 
-$db = new DataBase();
-$db->get_result("CREATE TABLE IF NOT EXISTS `job_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_title` varchar(255) NOT NULL,
-  `departments` varchar(255) NOT NULL,
-  `number_of_employees` int(11) DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4");
-
 $list_obj = new job_roles_details_LIST();
 if (isset($_GET['search']) && !empty($_GET['search'])) {
     $list_obj->filter_by_search($_GET['search']);
