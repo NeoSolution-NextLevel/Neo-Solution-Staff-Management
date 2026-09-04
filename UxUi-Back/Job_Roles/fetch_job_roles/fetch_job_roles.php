3890a13eeaa9f2adcba3620f7052b38f81490995
devindirathuge <devindineosolution@gmail.com>
2026-09-04 21:02:37 +0530

    Update fetch_job_roles.php

--- UxUi-Back/Job_Roles/fetch_job_roles/fetch_job_roles.php
+++ UxUi-Back/Job_Roles/fetch_job_roles/fetch_job_roles.php
@@ -3,6 +3,13 @@ header('Content-Type: application/json; charset=utf-8');
 
 include_once __DIR__ . '/../../../imports/need/DB.php';
 include_once __DIR__ . '/../../../Controllers/Main/Job_Roles/job_roles_details_LIST.php';
+include_once __DIR__ . '/../sync_job_roles_count.php';
+
+$db = new DataBase();
+$conn = $db->get_data_base_connction();
+if ($conn) {
+    sync_job_role_employee_counts($conn);
+}
 
 $list_obj = new job_roles_details_LIST();
 if (isset($_GET['search']) && !empty($_GET['search'])) {
