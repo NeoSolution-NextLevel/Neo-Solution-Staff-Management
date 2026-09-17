2834318dab2c74ce96b5255e1828dba54689ec56
devindirathuge <devindineosolution@gmail.com>
2026-09-17 14:30:10 +0530

    Update Successful-Page.php

--- UxUi/Main/Successful-Page.php
+++ UxUi/Main/Successful-Page.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
 include_once '../../imports/need/session_setup.php';
+require_login();
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
